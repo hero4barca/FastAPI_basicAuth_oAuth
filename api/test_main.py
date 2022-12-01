90526bafc6a06044b055de9c9d525dea99444e90
Hero <hero4barca@gmail.com>
2022-12-01 19:41:38 +0100

    configure different test environment

--- api/test_main.py
+++ api/test_main.py
@@ -1,6 +1,35 @@
+import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from .main import app
+from .main import app, get_db
+from .models import Base
+# from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///test_db.db'
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+app.dependency_overrides[get_db] = override_get_db
+
 
 client = TestClient(app)
 
@@ -9,18 +38,28 @@ def test_api_root():
     assert response.status_code == 200
     assert response.json() == "Todo api root"
 
+# def test_complete(test_db):
+#         payload = {
+#             "fname": "test_fname",
+#             "lname": "test_lname",
+#             "password": "password",
+#             "email": 'test_email@example.com'
+#         }
+#         response = client.post("/api/users",
+#                                      json=payload,  
+#                                      )
+#         assert response.status_code == 200
 
 class TestClassSignup:
+    
            
-    client = TestClient(app)
-
     def test_missing_lname_input_returns_error(self):
         payload = {
             "fname": "test_fname",
             "password": "password",
             "email": 'test_email@example.com'
         }
-        response = self.client.post("/api/users", json=payload)
+        response = client.post("/api/users", json=payload)
         assert response.status_code == 422
         response_detail = response.json()['detail']
         # print(response_detail)
@@ -35,7 +74,7 @@ class TestClassSignup:
             "password": "password",
             "email": 'test_email@example.com'
         }
-        response = self.client.post("/api/users", json=payload)
+        response = client.post("/api/users", json=payload)
         assert response.status_code == 422
         response_detail = response.json()['detail']
         assert response_detail[0]['msg'] == "field required"
@@ -50,14 +89,14 @@ class TestClassSignup:
             "password": "password",
             
         }
-        response = self.client.post("/api/users", json=payload)
+        response = client.post("/api/users", json=payload)
         assert response.status_code == 422
         response_detail = response.json()['detail']
         assert response_detail[0]['msg'] == "field required"
         assert response_detail[0]['type'] == "value_error.missing"
         assert response_detail[0]['loc'] == ["body","email"]
 
-
+    @pytest.mark.usefixtures("test_db")
     def test_complete(self):
         payload = {
             "fname": "test_fname",
@@ -65,12 +104,9 @@ class TestClassSignup:
             "password": "password",
             "email": 'test_email@example.com'
         }
-        response = self.client.post("/api/users",
+        response = client.post("/api/users",
                                      json=payload,  
                                      )
-       
-        response_detail = response.json()['detail']
-        print(response_detail)
         assert response.status_code == 200
        
 
