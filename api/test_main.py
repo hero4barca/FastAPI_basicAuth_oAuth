6f7dea4e89f584def62c14f848b07cf41b82e556
Hero <hero4barca@gmail.com>
2022-12-02 12:17:00 +0100

    improved test environmnet config for speed

--- api/test_main.py
+++ api/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
 from .main import app, get_db
@@ -9,51 +9,82 @@ from .models import Base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///test_db.db'
 
-engine = create_engine(
+engine = sa.create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
+# Set up the database once
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+# These two event listeners are only needed for sqlite for proper
+# SAVEPOINT / nested transaction support. Other databases like postgres
+# don't need them. 
+# From: https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#serializable-isolation-savepoints-transactional-ddl
+@sa.event.listens_for(engine, "connect")
+def do_connect(dbapi_connection, connection_record):
+    # disable pysqlite's emitting of the BEGIN statement entirely.
+    # also stops it from emitting COMMIT before any DDL.
+    dbapi_connection.isolation_level = None
+
+@sa.event.listens_for(engine, "begin")
+def do_begin(conn):
+    # emit our own BEGIN
+    conn.exec_driver_sql("BEGIN")
 
 
+# This fixture is the main difference to before. It creates a nested
+# transaction, recreates it when the application code calls session.commit
+# and rolls it back at the end.
+# Based on: https://docs.sqlalchemy.org/en/14/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
 @pytest.fixture()
-def test_db():
-    Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
+def session():
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
 
-app.dependency_overrides[get_db] = override_get_db
+    # Begin a nested transaction (using SAVEPOINT).
+    nested = connection.begin_nested()
+
+    # If the application code calls session.commit, it will end the nested
+    # transaction. Need to start a new one when that happens.
+    @sa.event.listens_for(session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        nonlocal nested
+        if not nested.is_active:
+            nested = connection.begin_nested()
+
+    yield session
+
+    # Rollback the overall transaction, restoring the state before the test ran.
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+# A fixture for the fastapi test client which depends on the
+# previous session fixture. Instead of creating a new session in the
+# dependency override as before, it uses the one provided by the
+# session fixture.
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        yield session
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    del app.dependency_overrides[get_db]
 
 
-client = TestClient(app)
-
-def test_api_root():
+def test_api_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Todo api root"
 
-# def test_complete(test_db):
-#         payload = {
-#             "fname": "test_fname",
-#             "lname": "test_lname",
-#             "password": "password",
-#             "email": 'test_email@example.com'
-#         }
-#         response = client.post("/api/users",
-#                                      json=payload,  
-#                                      )
-#         assert response.status_code == 200
 
-class TestClassSignup:
-    
+class TestClassSignup:   
            
-    def test_missing_lname_input_returns_error(self):
+    def test_missing_lname_input_returns_error(self, client):
         payload = {
             "fname": "test_fname",
             "password": "password",
@@ -68,7 +99,7 @@ class TestClassSignup:
         assert response_detail[0]['loc'] == ["body","lname"]
 
 
-    def test_missing_fname_input_returns_error(self):
+    def test_missing_fname_input_returns_error(self,client):
         payload = {
             "lname": "test_lname",
             "password": "password",
@@ -82,7 +113,7 @@ class TestClassSignup:
         assert response_detail[0]['loc'] == ["body","fname"]
 
 
-    def test_missing_email_input_returns_error(self):
+    def test_missing_email_input_returns_error(self,client):
         payload = {
             "lname": "test_lname",
             "fname": "test_fname",
@@ -96,8 +127,7 @@ class TestClassSignup:
         assert response_detail[0]['type'] == "value_error.missing"
         assert response_detail[0]['loc'] == ["body","email"]
 
-    @pytest.mark.usefixtures("test_db")
-    def test_complete(self):
+    def test_complete(self,client):
         payload = {
             "fname": "test_fname",
             "lname": "test_lname",
